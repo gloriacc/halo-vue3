745b8f176b3417a07bd37380c08a35587a5237e0
gloriacc <xuan_1213@yeah.net>
2020-07-29 20:30:54 +0800

    home 样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,8 +3,8 @@
   <div class="banner">
     <h1>Halo UI</h1>
     <p class="actions">
-      <a>GitHub</a>
-      <a>开始</a>
+      <a href="https://github.com">GitHub</a>
+      <router-link to="/doc">开始</router-link>
     </p>
   </div>
 </template>
@@ -16,5 +16,24 @@
   };
 </script>
 <style lang="scss" scoped>
-
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  > .actions{
+    padding: 8px 0;
+    a {
+      margin:0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
+}
 </style>
