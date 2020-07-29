59cb6153b84bc7c3dbb938dca03fa89059ac7973
gloriacc <xuan_1213@yeah.net>
2020-07-29 20:39:04 +0800

    aside 样式

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,7 +1,23 @@
 <template>
   <Nav></Nav>
   <div class="content">
-    <aside>aside</aside>
+    <aside>
+      <h2>组件列表</h2>
+      <ol>
+        <li>
+          <router-link to="/doc/switch">Switch 组件</router-link>
+        </li>
+        <li>
+          <router-link to="/doc/button">Button 组件</router-link>
+        </li>
+        <li>
+          <router-link to="/doc/dialog">Dialog 组件</router-link>
+        </li>
+        <li>
+          <router-link to="/doc/tabs">Tabs 组件</router-link>
+        </li>
+      </ol>
+    </aside>
     <main>main</main>
   </div>
 </template>
@@ -12,3 +28,20 @@
     components: {Nav}
   };
 </script>
+<style lang="scss" scoped>
+aside {
+  width: 150px;
+  padding: 70px 16px 16px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  > h2 {
+    margin-bottom: 4px;
+  }
+  > ol {
+    > li {
+      padding: 4px 0;
+    }
+  }
+}
+</style>
