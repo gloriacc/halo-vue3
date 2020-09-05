b3b32bf88c2d6c2b9a2dc962c3e10384b3aad92b
gloriacc <xuan_1213@yeah.net>
2020-09-05 23:44:58 +0800

    button 部分测试

--- src/test/button.spec.ts
+++ src/test/button.spec.ts
@@ -1,9 +1,47 @@
-import {mount, VueWrapper} from '@vue/test-utils';
+import {mount} from '@vue/test-utils';
 import Button from '../lib/button.vue'
 
 describe('Button.vue', () => {
-  it('render a button', () => {
-    const wrapper: VueWrapper<any> = mount(Button)
-    expect(1).toBe(2)
+  it('could render', () => {
+    // @ts-ignore
+    const wrapper = mount(Button)
+    expect(wrapper.html()).not.toBeUndefined()
+  })
+  it('could set custom color', () => {
+    const expectedColor = '#008000'
+    // @ts-ignore
+    const wrapper = mount(Button, {
+      props: {
+        color: expectedColor
+      }
+    })
+    expect(wrapper.attributes().style).toMatch(expectedColor)
+  })
+  it('could be disabled', async () => {
+    // @ts-ignore
+    const wrapper = mount(Button)
+    expect(wrapper.classes()).not.toContain('halo-button-disabled')
+    await wrapper.setProps({
+      disabled: true
+    })
+    expect(wrapper.classes()).toContain('halo-button-disabled')
+  })
+  it('could be round-shape', async () => {
+    // @ts-ignore
+    const wrapper = mount(Button)
+    expect(wrapper.classes()).not.toContain('halo-button-round')
+    await wrapper.setProps({
+      shape: 'round'
+    })
+    expect(wrapper.classes()).toContain('halo-button-round')
+  })
+  it('could be circle-shape', async () => {
+    // @ts-ignore
+    const wrapper = mount(Button)
+    expect(wrapper.classes()).not.toContain('halo-button-circle')
+    await wrapper.setProps({
+      shape: 'circle'
+    })
+    expect(wrapper.classes()).toContain('halo-button-circle')
   })
 })
