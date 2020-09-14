db81319b65f3b8f2ccb2664dad4747907360aef1
gloriacc <xuan_1213@yeah.net>
2020-09-14 18:25:23 +0800

    dist

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-const { build } = require('vite')
-;(async () => {
-  const result = await build({
-    rollupInputOptions: {
-      input: 'index.js',
-    },
-    rollupOutputOptions: {
-      file: 'dist/index.es.js',
-      format: 'es',
-    },
-    cssCodeSplit: false,
-    assetsDir: '.',
-    minify: false,
-  })
-})()
+// const { build } = require('vite')
+// ;(async () => {
+//   const result = await build({
+//     rollupInputOptions: {
+//       input: 'index.js',
+//     },
+//     rollupOutputOptions: {
+//       file: 'dist/index.es.js',
+//       format: 'es',
+//     },
+//     cssCodeSplit: false,
+//     assetsDir: '.',
+//     minify: false,
+//   })
+// })()
